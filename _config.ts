783dac5428286bd20a23a6a43db22d44ba42066b
abetomoki0420 <kimi.jack.of.all.trades@gmail.com>
2026-03-28 10:51:37 +0900

    fix

--- _config.ts
+++ _config.ts
@@ -2,7 +2,7 @@ import lume from "lume/mod.ts";
 import blog from "blog/mod.ts";
 
 const site = lume({
-  location: new URL("https://abetomoki0420.github.io/github-pages-blog/"),
+  location: new URL("https://abetomoki0420.github.io/"),
 });
 
 site.use(blog());
